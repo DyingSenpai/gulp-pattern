6d8863f23c8baa26869f2fb4c70946bf5b2b9f01
Senpai <ratulird@gmail.com>
2021-08-26 23:27:59 +0300

    Delete webp plugin

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,6 @@ const clean_css = require("gulp-clean-css");
 const rename = require("gulp-rename");
 const uglify = require("gulp-uglify-es").default;
 const imagemin = require("gulp-imagemin"); // Old version
-const webp = require("gulp-webp");
 
 /* Path*/
 let project_folder = "dist/";
@@ -108,13 +107,6 @@ function js() {
 
 function images() {
     return src(path.src.img)
-        .pipe(
-            webp({
-                quality: 70
-            })
-        )
-        .pipe(dest(path.build.img))
-        .pipe(src(path.src.img))
         .pipe(imagemin([
             imagemin.gifsicle({ interlaced: true }),
             imagemin.mozjpeg({ quality: 95, progressive: true }),
